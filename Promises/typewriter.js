ef8099c2c168b41e814d4fcf16e7067bb88dbb0d
Wesley Cantrell <mail@init1.io>
2021-11-09 10:20:59 -0600

    update

---
+++ Promises/typewriter.js
@@ -0,0 +1,45 @@
+const h1 = document.querySelector('h1');
+const h2 = document.querySelector('h2');
+const h3 = document.querySelector('h3');
+const h4 = document.querySelector('h4');
+const p1 = document.querySelector('.p1');
+const p2 = document.querySelector('.p2');
+
+function wait(ms = 0) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+
+
+
+
+
+
+// async function typeText(el, delay) {
+//     const textArr = [...el.textContent];
+//     el.textContent = '';
+
+//     for (let char of textArr) {
+//         el.textContent += char;
+//         await wait(delay);
+//     }
+// }
+
+// typeText(h1,500);
+// typeText(h2,400);
+// typeText(h3,300);
+// typeText(h4,200);
+// typeText(p1,50);
+// typeText(p2,25);   
+
+
+
+
+
+
+
+
+
+
